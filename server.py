834fd5523b2e7d820ff66ee6f7cbf9b582fea4e8
Christopher-Teague <cmteague77@gmail.com>
2021-11-03 22:51:41 -0700

    tinkering

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request ,redirect
+from flask.wrappers import Request
 from mysqlconnection import connectToMySQL
 # import the class from user.py
 from user import Users
@@ -7,14 +8,25 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
+    return redirect('/users')
+
+@app.route('/users')
+def users():
     users = Users.get_all()
     print(users)
     return render_template("read_all.html", all_users = users)
 
-@app.route('/create')
-def create(cls, data):
-    query = "INSERT INTO users (first_name, last_name, email) VALUES (%(first_name)s, %(last_name)s, %(email)s)"
-    return connectToMySQL(DATABASE).query_db(query, data)
+@app.route('/user/new')
+def new():
+    return render_template("create.html")
+
+@app.route('/user/create', methods=['POST'])
+def create():
+    print(request.form)
+    Users.save(request.form)
+    return redirect('/users')
+    # query = "INSERT INTO users (first_name, last_name, email) VALUES (%(first_name)s, %(last_name)s, %(email)s)"
+    # return connectToMySQL(DATABASE).query_db(query, data)
 
             
 if __name__ == "__main__":
